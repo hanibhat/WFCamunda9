a9c909697650fdcb546fff1476b9c502c3cd430d
h_bhat01 <h_bhat01@uni-muenster.de>
2017-06-15 15:24:25 +0200

    pinkblob app

---
+++ src/main/java/org/camunda/bpm/getstarted/pinkblob/pinkblobApplication.java
@@ -0,0 +1,10 @@
+package org.camunda.bpm.getstarted.pinkblob;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Pinkblob App")
+public class pinkblobApplication extends ServletProcessApplication {
+	  // empty implementation
+	}
+

d8fcc346de137e5ab1c48f6956c3b096601ed3cd
h_bhat01 <h_bhat01@uni-muenster.de>
2017-07-20 15:03:36 +0200

    Create request and initiate process from frontend "localhost:8010/pink_blob/index.html"

---
+++ src/main/java/org/camunda/bpm/pinkblob/servlet/CreateRequestServlet.java
@@ -0,0 +1,68 @@
+package org.camunda.bpm.pinkblob.servlet;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/createRequest")
+public class CreateRequestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        // read form fields
+        String corrID = request.getParameter("corrID");
+        String name = request.getParameter("name");
+        String company = request.getParameter("company");
+        String address = request.getParameter("address");
+        String emailAdd = request.getParameter("emailAdd");
+        String phone = request.getParameter("name");
+        String classifiedRequirements = request.getParameter("classifiedRequirements");
+        String hardcopyRequired = request.getParameter("hardcopyRequired");
+
+        System.out.println("name: " + name);
+        System.out.println("company: " + company);
+        System.out.println("address: " + address);
+        System.out.println("email: " + emailAdd);
+        System.out.println("phone number: " + phone);
+        System.out.println("Classified Requirements: " + classifiedRequirements);
+        System.out.println("Hardcopy: " + hardcopyRequired);
+
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        PrintWriter out = response.getWriter();
+        ProcessInstance processInstance;
+        if (null == corrID) {
+            processInstance = runtimeService.startProcessInstanceByMessage("InstantiationMessage2");
+        } else {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("corrID", corrID);
+            map.put("company", company);
+            map.put("address", address);
+            map.put("phone", phone);
+            map.put("classifiedRequirements", classifiedRequirements);
+            map.put("hardcopyRequired", hardcopyRequired);
+            processInstance = runtimeService.startProcessInstanceByMessage("InstantiationMessage2", map);
+        }
+        out.println("<html><body>");
+        out.println("<h1>Process Instance Started</h1>");
+        out.println("<p>ID: " + processInstance.getId() + "</p>");
+        out.println("<p>corrID: " + corrID + "</p>");
+        out.println("</body></html>");
+        
+//        redirect to thank u page
+
+    }
+
+
+}
--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -42,4 +42,8 @@ $(document).ready(function() {
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
+    $(document).ready(function(){
+        $('.parallax').parallax();
+    });
 });
+
